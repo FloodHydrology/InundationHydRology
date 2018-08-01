976c3d46bd68b5ae90d900804151b1ddd0512443
Nate Jones <njones@sesync.org>
2018-07-31 22:24:57 -0400

    dealt with NA

--- R/GIW_subshed_delineation.R
+++ R/GIW_subshed_delineation.R
@@ -85,7 +85,7 @@ GIW_subshed_delineation<-function(
   
   #If there are other basins complete the analysis again to remove
   depressions[depressions==raster::extract(depressions, wetland)]<-NA
-  n_depression<-length(unique(depressions@data@values))
+  n_depression<-length(unique(depressions@data@values)-1)
   while(n_depression>0){
     #Extract depression of interest
     wetland_dep<-depressions
@@ -117,7 +117,7 @@ GIW_subshed_delineation<-function(
     
     #Recalculate nubmer of depressions
     depressions<-depressions*watershed
-    n_depression<-length(unique(depressions@data@values))
+    n_depression<-length(unique(depressions@data@values)-1)
   }
   
   #Export Watershed
